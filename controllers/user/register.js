8fa149a48005fc18dc93da4eebfb1b0980bf7f8e
maainul <mainul080@yahoo.com>
2023-02-11 01:27:46 +0600

    Add logger

--- controllers/user/register.js
+++ controllers/user/register.js
@@ -1,12 +1,15 @@
+import logger from "../../logger/logger.js";
 import User from "../../models/user.model.js";
 
 const register = async (req, res) => {
   try {
     await User.create(req.body);
+    logger.info("User create called.")
     res.json({
       message: "User Created Successfully.",
     });
   } catch (error) {
+    logger.error("Error while add user data.")
     res.json({ message: error.message });
   }
 };
